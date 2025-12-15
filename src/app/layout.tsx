b1b1558ae16b60fcdb001236ed2ab48a5059258a
Licrua <ilay1275@mail.ru>
2025-12-15 16:25:07 +0300

    yandex metrics integrate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,19 +7,54 @@ import "./styles/globals.css";
 
 export const metadata: Metadata = {
   title: "LictuaPortfolio",
-  description: "website represents porftolio with works, experiance and main stack ",
+  description: "website represents portfolio with works, experience and main stack",
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const locale = await getLocale();
-
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
 
   return (
     <html lang={locale}>
-      <body className={`${sen.className}`}>
+      <head>
+        {/* Yandex.Metrika */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {
+    if (document.scripts[j].src === r) { return; }
+  }
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+  k.async=1;k.src=r;a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105853093', 'ym');
+
+ym(105853093, 'init', {
+  ssr: true,
+  webvisor: true,
+  clickmap: true,
+  ecommerce: "dataLayer",
+  accurateTrackBounce: true,
+  trackLinks: true
+});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/105853093"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika */}
+      </head>
+
+      <body className={sen.className}>
         <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
       </body>
     </html>
